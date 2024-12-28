fd44658e044a10a476f47e2999c59800a9d4d5c8
liliana <lilianasalina1993@gmail.com>
2024-12-28 01:44:38 -0500

    se ajustaron variables del modelo clientes

--- app/models/clientes.py
+++ app/models/clientes.py
@@ -5,12 +5,12 @@ class Cliente(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
-    correo = db.Column(db.String(100), nullable=False, unique=True)  # Asegúrate de que este campo existe
+    email = db.Column(db.String(100), nullable=False, unique=True)  # Asegúrate de que este campo existe
     whatsapp = db.Column(db.String(15), nullable=True)
 
     def __init__(self, nombre, correo, whatsapp=None):
         self.nombre = nombre
-        self.correo = correo
+        self.email = correo 
         self.whatsapp = whatsapp
 
     def __repr__(self):
